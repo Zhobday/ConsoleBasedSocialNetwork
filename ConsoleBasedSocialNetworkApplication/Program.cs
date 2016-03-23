20f87bcd0105fd065391950f9cec791b4b2a57c4
Zac <zachobday0@gmail.com>
2016-03-23 16:09:14 +0000

    fixed exit command

--- ConsoleBasedSocialNetworkApplication/Program.cs
+++ ConsoleBasedSocialNetworkApplication/Program.cs
@@ -25,7 +25,20 @@ namespace ConsoleBasedSocialNetworkApplication
                 {
                     socialNetwork.Post(input);
                 }
-                else if (!input.Contains(" ") && input.ToLower() != "help")
+                else if (input.ToLower() == "exit")
+                {
+                    quitNow = true;
+                }
+                else if (input.ToLower() == "help")
+                {
+                    Console.WriteLine("To post a message type: <user name> -> <message>");
+                    Console.WriteLine("To read an existing users timeline type: <user name>");
+                    Console.WriteLine("To follow another user type: <user name> follows <another user>");
+                    Console.WriteLine("To view a wall type:  <user name> wall");
+                    Console.WriteLine("Dont worry if you're not an existing user, new user names will be added on commands!");
+                    Console.WriteLine("If you're done having fun and want to leave type: exit");
+                }
+                else if (!input.Contains(" "))
                 {
                     var user = socialNetwork.Read(input);
 
@@ -47,20 +60,8 @@ namespace ConsoleBasedSocialNetworkApplication
                         Console.WriteLine("{0} - {1}",post.Name,post.GetFullPost());
                     }
                 }
-                else if(input.ToLower() == "help")
-                {
-                    Console.WriteLine("To post a message type: <user name> -> <message>");
-                    Console.WriteLine("To read an existing users timeline type: <user name>");
-                    Console.WriteLine("To follow another user type: <user name> follows <another user>");
-                    Console.WriteLine("To view a wall type:  <user name> wall");
-                    Console.WriteLine("Dont worry if you're not an existing user, new user names will be added on commands!");
-                    Console.WriteLine("If you're done having fun and want to leave type: exit");
-                }
+                
 
-                else if (input.ToLower() == "exit")
-                {
-                    quitNow = true;
-                }
             }
 
         }
